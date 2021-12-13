2bacfb30ee2a284a7411932d2eb9ef0a5466f77f
Johns Raju <83854933+JOHNSRAJU@users.noreply.github.com>
2021-12-14 00:02:20 +0530

    Add files via upload

--- polynomialRepresentation.c
+++ polynomialRepresentation.c
@@ -4,21 +4,20 @@ struct polynomial{
 	int coefficient;
 	int exponent;
 };
-struct polynomial expon[10];
+struct polynomial expon[MAX];
 int main(){
 	int i,terms;
 	printf("Enter the  no.of terms\n");
 	scanf("%d",&terms);
 	for(i=0;i<terms;i++){
-		printf("\n\nEnter the coefficient of term %d :- ",i);
-		scanf("%d",&expon[i].coefficient);
-		printf("\nEnter the exponent of term %d :- ",i);
+        printf("\n\nEnter the exponent");
 		scanf("%d",&expon[i].exponent);
+		printf("Enter the coefficient of (x^%d) :- ",expon[i].exponent);
+		scanf("%d",&expon[i].coefficient);
+		
 	}
 	printf("Your Polynomial Expression is \n");
 	for(i=0;i<terms;i++){
 		printf("%d (x^%d) + ",expon[i].coefficient,expon[i].exponent);
 	}
 }
-
-
