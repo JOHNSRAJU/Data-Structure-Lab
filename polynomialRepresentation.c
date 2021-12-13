b037960bd427c9af79031e4c2ac63958bbf8b274
Johns Raju <83854933+JOHNSRAJU@users.noreply.github.com>
2021-12-13 14:06:39 +0530

    Add files via upload

--- polynomialRepresentation.c
+++ polynomialRepresentation.c
@@ -9,13 +9,13 @@ int main(){
 	int i,greatest;
 	printf("Enter the greatest expontial\n");
 	scanf("%d",&greatest);
-	for(i=greatest;i=>0;i--){
+	for(i=greatest;i>=0;i--){
 		printf("Enter the coefficient of x^(%d) :- ",i);
 		scanf("%d",&expon[i].coefficient);
 		expon[i].exponent=i;
 	}
 	printf("Your Polynomial Expression is \n");
-	for(i=greatest;i=>0;i--){
+	for(i=greatest;i>=0;i--){
 		printf("%d x^(%d) + ",expon[i].coefficient,expon[i].exponent);
 	}
 }
