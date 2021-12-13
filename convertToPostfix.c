baeebbb0643de90e963b57cbb83f38539228e366
Johns Raju <83854933+JOHNSRAJU@users.noreply.github.com>
2021-12-13 23:54:11 +0530

    To convert to postfix

--- convertToPostfix.c
+++ convertToPostfix.c
@@ -1,75 +1,74 @@
+/*****************************************************************************************************
+$ NAME - convertToPostfix.c                                                                          *           
+$ CREATOR - JOHNS RAJU                                                                               *
+$ PROGRAM -  To convert infix to postfix notation.                                                   *
+$ DATE - 13/12/2021                                                                                  *
+$ VERSION - 1.0                                                                                      *
+*****************************************************************************************************/
 #include<stdio.h>
 #include<ctype.h>
-#include<string.h>
-char myStack[20];
-int top;
-int max=20;
+#include<string.h> 
+int top=-1;
+char stack[20];
+
 void push(char);
-char pop();
-int priority(char);
-
+int pop();
+int privority(char);
 int main(){
-	int top=-1,i=0;
-	char myStack[100],infix[20],x;
-	printf("Enter the infix expression");
-	scanf("%s",infix);
-	
-	push('(');
-	while(infix[i]!='\0'){
-		if(isalnum(infix[i])){
-			printf("%c ",infix[i]);
-		}
-		else if(infix[i]=='('){
-			push(infix[i]);
-		}
-		
-		else if(infix[i]==')'){
-			
-			while(pop()!='('){
-				printf("%c ",x);
-			}
-		}
-		else{
-			if(priority(myStack[top])>=priority(infix[i])){
-				while(priority(myStack[top])>=priority(infix[i])){
-					printf("%c",pop());
-				}
-			}
-			push(infix[i]);
-		}
-		i++;
-	}
-	while(top!=-1){
-		pop();
-		top--;
-	}
-	return 0;
-}
-int priority(char element){
-	if(element == '('){
-		return 0;
-	}
-	else if(element=='+' || element=='-'){
-		return 1;
-	}
-	else if(element=='*' || element=='/'){
-		return 2;
-	}
-	else{
-		return 0;
-	}
-}
 
-void push(char value){
-
-	
-		top=top+1;
-		myStack[top]=value;
-	
-	
+    push('(');
+    char infix[20],*p;
+    printf("Enter the infix expression\n");
+    scanf("%s",infix);
+    p=infix;
+    printf("\nYour postfix expression is\n");
+    while(*p!='\0'){
+        if(isalnum(*p)){
+            printf("%c ",*p);
+        }
+        else if(*p=='('){
+            push(*p);
+        }
+        else if(*p==')'){
+            while(pop()!='(');
+        }
+        else{
+            while(privority(stack[top])>=privority(*p)){
+                pop();
+            }
+            push(*p);
+        }
+    p++;
+    }
+    while(top>=0){
+        pop();
+    }
+    return 0;
 }
-char pop(){
-	printf("%c",myStack[top]);
-	return myStack[top--];
-	
+void push(char pushElement){
+    top++;
+    stack[top]=pushElement;
+}
+int pop(){
+    char x;
+    x=stack[top];
+    if(x!='('){
+        printf("%c ",x);
+    } 
+    top--;
+    return x;
+}
+int privority(char element){
+    if(element=='^'){
+        return 3;
+    }
+    else if(element=='*' || element=='/'){
+        return 2;
+    }
+    else if(element=='+' || element=='-'){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
